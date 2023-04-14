b5594e65bcce8829393ffd8f9b26f30d5b605c56
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2023-04-14 20:00:33 +0200

    feat: add navbar links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useSupabaseClient, useSession } from "@supabase/auth-helpers-react";
 import { Button } from "@mui/material";
+import Link from "next/link";
 
 export default function Navbar() {
   const session = useSession();
@@ -12,9 +13,12 @@ export default function Navbar() {
         <Button variant="contained" onClick={() => supabase.auth.signOut()}>
           Sign out
         </Button>
-      ) : (
-        <Button>sign in</Button>
-      )}
+      ) : null}
+
+      <Link href="/profile">dashboard</Link>
+      <Link href="/profile/addBook">add</Link>
+      <Link href="/profile/library">your library</Link>
+      <Link href="/profile/achievements">achievements</Link>
     </div>
   );
 }
